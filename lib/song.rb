fd8ecd9b3d32d8e05d19a2788a06ebffddb215de
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-21 14:38:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,55 @@
 # class Song
-# 
+#
 #   attr_accessor :artist, :name, :genre
-# 
+#
 #   @@all = []
-# 
+#
 #   def initialize(name)
 #       @name = name
 #       save
 #     end
-# 
+#
 #     def name=(name)
 #       @name = name
 #     end
-# 
+#
 #     def artist=(artist)
 #       @artist = artist
 #     end
-# 
+#
 #     def artist_name
 #       self.artist.name
 #     end
-# 
-# 
+#
+#
 #     def songs
 #       Song.all.select {|song| song.artist == self}
 #     end
-# 
+#
 #     def save
 #       @@all << self
 #     end
-# 
+#
 #     def self.all
 #       @@all
 #     end
-# 
+#
 #     def artist_name=(name)
-# 
+#
 #       if (self.artist.nil?)
 #         self.artist = Artist.new(name)
 #       else
 #         self.artist.name = name
 #       end
 #     end
-# 
+#
 #     def self.new_by_filename(filename)
 #       artist_name, song_name, extra = filename.split(" - ")
 #       song = self.new(song_name)
 #       artist = Artist.find_or_create_by_name(artist_name)
 #       artist.add_song(song)
 #     end
-# 
+#
 #     def artist_name=(name)
 #       self.artist = Artist.find_or_create_by_name(name)
 #       self.artist.add_song(self)
